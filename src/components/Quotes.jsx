10088d36811223c7ce5c6e35b6f50639850c7ba6
Lindsay Trenton <lt353@hawaii.edu>
2025-04-23 14:13:38 -1000

    Finished all tasks to make the app functional

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,39 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../Quotes.css";
 
 // TODO: Import useEffect
 
 function Quotes(props) {
 	// TODO: Remove hard-coded quotes
-	const [quotes, setQuotes] = useState([
-		{
-			quote: "Be nice to nerds. You may end up working for them. We all could.",
-			source: "Charles J. Sykes",
-		},
-		{
-			quote: "The opposite of love is not hate, it's indifference.",
-			source: "Elie Wiesel",
-		},
-		{
-			quote: "The unexamined life is not worth living.",
-			source: "Socrates",
-		},
-	]);
+	const [quotes, setQuotes] = useState([]);
+	const [error, setError] = useState("");
+	const [loading, setLoading] = useState(true);
 
 	// TODO: Add other state variables here
 
 	// TODO: Call useEffect() to fetch quotes here
+	useEffect(() => {
+		async function fetchQuotes() {
+			setQuotes([]);
+			setError("");
+			setLoading(true);
 
+			const url = `https://wp.zybooks.com/quotes.php?topic=${props.topic.toLowerCase()}&count=${
+				props.count
+			}`;
+			const response = await fetch(url);
+
+			if (response.ok) {
+				const result = await response.json();
+				if (Array.isArray(result)) {
+					setQuotes(result);
+				} else if (result.quotes) {
+					setQuotes(result.quotes);
+				} else if (result.error) {
+					setError(result.error);
+				} else {
+					setError("Unexpected response format from server");
+				}
+			} else {
+				setError(`Topic '${props.topic}' not found`);
+			}
+			setLoading(false);
+		}
+		fetchQuotes();
+	}, [props.topic, props.count]);
 	// TODO: Render error message and loading message
 	return (
 		<section>
-			<ol>
-				{quotes.map((q, index) => (
-					<li key={index}>
-						{q.quote} - {q.source}
-					</li>
-				))}
-			</ol>
+			{error && <p className="error">{error}</p>}
+			{loading ? (
+				<p>Loading...</p>
+			) : (
+				<ol>
+					{quotes.map((q, index) => (
+						<li key={index}>
+							{q.quote} - {q.source}
+						</li>
+					))}
+				</ol>
+			)}
 		</section>
 	);
 }
